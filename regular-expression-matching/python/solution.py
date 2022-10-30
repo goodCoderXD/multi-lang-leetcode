c570b753e58830848f4f8d68ff18439c9ba713cc
GoodCoderXD <npengra@nvidia.com>
2022-10-30 12:02:43 -0700

    add guards

--- regular-expression-matching/python/solution.py
+++ regular-expression-matching/python/solution.py
@@ -1,7 +1,14 @@
 from functools import lru_cache
 
-@lru_cache
 def is_match(string: str, pattern: str) -> bool:
+    if not pattern or "**" in pattern or pattern[0] == "*":
+        raise ValueError("Illegal Pattern")
+    
+    return _is_match(string, pattern)
+
+
+@lru_cache
+def _is_match(string: str, pattern: str) -> bool:
     if not pattern:
         return not string
 
@@ -9,10 +16,10 @@ def is_match(string: str, pattern: str) -> bool:
 
     if len(pattern) >= 2 and pattern[1] == "*":
         return (
-            (first_match and is_match(string[1:], pattern)) or is_match(string, pattern[2:])
+            (first_match and _is_match(string[1:], pattern)) or _is_match(string, pattern[2:])
         )
     else:
-        return first_match and is_match(string[1:], pattern[1:])
+        return first_match and _is_match(string[1:], pattern[1:])
 
 
     
